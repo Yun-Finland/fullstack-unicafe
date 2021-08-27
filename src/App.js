7392d15dce823d728e605596b4950b70a2795ecc
Yun Xiao <yun.xiao.finland@gmail.com>
2021-08-27 20:33:14 +0300

    simplified functions

--- src/App.js
+++ src/App.js
@@ -33,30 +33,16 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
-const Button = ({label, choice, setChoice}) => {
-  return (
-    <div>
-      <button onClick={setChoice}>{label}</button>
-    </div>
-  )
-}
+const Button = ({label, choice, setChoice}) => <button onClick={setChoice}>{label}</button>
 
 const App = () => {
   const [ good, setGood ] = useState(0);
   const [ neutral, setNeutral ] = useState(0);
   const [ bad, setBad ] = useState(0);
   
-  const voteGood = () =>{
-    setGood(good+1);
-  }
-
-  const voteNeutral = () =>{
-    setNeutral(neutral+1);
-  }
-
-  const voteBad = () =>{
-    setBad(bad+1);
-  }
+  const voteGood = () => setGood(good+1);
+  const voteNeutral = () => setNeutral(neutral+1);
+  const voteBad = () => setBad(bad+1);
 
   return (
     <div>
